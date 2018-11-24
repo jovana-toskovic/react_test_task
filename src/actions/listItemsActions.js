75f1752d5a6fcb503af4b96c642734b310a1624e
Jovana Toskovic <toskovic.jovana@gmail.com>
2018-11-24 16:32:17 +0100

    Adding Redux

---
+++ src/actions/listItemsActions.js
@@ -0,0 +1,6 @@
+import { 
+  ALL_LIST_ITEMS, 
+  ADD_LIST_ITEM, 
+  EDIT_LIST_ITEM, 
+  DELETE_LIST_ITEM  
+} from './types';
---
+++ src/reducers/listItemsReducer.js
@@ -0,0 +1,10 @@
+import { 
+  ALL_LIST_ITEMS, 
+  ADD_LIST_ITEM, 
+  EDIT_LIST_ITEM, 
+  DELETE_LIST_ITEM 
+} from './../actions/types.js'
+
+const initialState = {
+  items: []
+}
